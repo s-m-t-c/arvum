88645a58ad6911e77b9ed51183f8e488c2d61698
Sean <sean.chua@ga.gov.au>
2020-12-16 17:06:14 +1100

    small changes

--- sample.py
+++ sample.py
@@ -1,5 +1,5 @@
 import pandas as pd
 import numpy as np
-data = pd.read_csv("2010_2015_median_training_data.txt", header=0, sep=" ")
-data['binary_class'] = np.where(data['classnum'] == 111, 111, 0)
-data.to_csv("2010_2015_median_training_data_binary.txt", sep=" ", index=False)
+data = pd.read_csv("/g/data/r78/LCCS_Aberystwyth/training_data/cultivated/2010_2015_training_data_combined_23102020/2015_median_training_data_indices.txt", header=0, sep=" ")
+data['binary_class'] = np.where(data['classnum'] == 111, 1, 0)
+data.to_csv("2015_median_training_data_binary_test.txt", sep=" ", index=False)
