8e3a02c053af518486b14d934aabcd73d12e529f
s-m-t-c <sean.chua@ga.gov.au>
2021-02-16 14:59:27 +1100

    cleaned up custom data extraction

--- extract_data_for_shp_custom.py
+++ extract_data_for_shp_custom.py
@@ -15,31 +15,30 @@ import datacube
 sys.path.append("/g/data/u46/users/sc0554/dea-notebooks/Scripts")
 from dea_bandindices import calculate_indices
 import xarray as xr
-#sys.path.append("/g/data/u46/users/sc0554/deafrica-sandbox-notebooks/Scripts")
-#sys.path.append("/g/data/u46/users/sc0554/deafrica-sandbox-notebooks/crop_mask")
-#from deafrica_temporal_statistics import temporal_statistics, xr_phenology
-#from feature_layer_extractions import xr_terrain
-# Assume all repos are checked out to same location so get relative to this.
-sys.path.append("/g/data/u46/users/sc0554/dea-notebooks/Scripts/")
 from dea_classificationtools import collect_training_data
 
-path = "/g/data/r78/LCCS_Aberystwyth/training_data/cultivated/2015_merged/2015_merged.shp"
+path = "/g/data/r78/LCCS_Aberystwyth/training_data/cultivated/2010_merged/2010_merged.shp"
 field = "classnum"
-products = ["ls8_nbart_geomedian_annual"]
-time = "2015"
+# Need ls5 for 2010 and ls8 for 2015+
+products = ["ls5_nbart_geomedian_annual"]
+time = "2010"
 zonal_stats = 'median'
 resolution = (-30, 30)
-ncpus = 48 
+ncpus = 1 
 reduce_func = None  #'geomedian'
 band_indices = None  # ['NDVI']
 drop = False
 input_data = gpd.read_file(path)
+clean=True
+return_coords=True
 
 def custom_function(ds):
-    gm = calculate_indices(ds, index=["NDVI", "MNDWI", "BAI", "BUI", "BSI", "TCG", "TCW", "TCB", "NDMI", "LAI", "EVI", "AWEI_sh", "BAEI", "NDSI", "SAVI"], drop=False, collection="ga_ls_2")
+    gm = calculate_indices(ds, index=["NDVI", "MNDWI", "BAI", "BUI", "BSI", "TCG", "TCW", "TCB", "NDMI", "LAI", "EVI", "AWEI_sh", "BAEI", "NDSI", "SAVI", "NBR"], drop=False, collection="ga_ls_2")
     dc = datacube.Datacube(app='custom_function')
-    mad = dc.load(product='ls8_nbart_tmad_annual', like=ds)
-    output = xr.merge([gm, mad])
+    # Need ls5 for 2010 and ls8 for 2015+
+    mad = dc.load(product='ls5_nbart_tmad_annual', like=ds)
+    fc = dc.load(product='fc_percentile_albers_annual', like=ds)
+    output = xr.merge([gm, mad, fc])
     return output
 
 query = {
@@ -54,15 +53,17 @@ column_names, model_input = collect_training_data(
     products=products,
     dc_query=query,
     ncpus=ncpus,
+    return_coords=return_coords,
     custom_func=custom_function,
     field=field,
     calc_indices=band_indices,
     reduce_func=reduce_func,
     drop=drop,
     zonal_stats=zonal_stats,
+    clean=clean
 )
 
 print(model_input.shape)
-output_file = "2015_median_training_data_indices.txt"
+output_file = "2010_median_training_data_indices.txt"
 
 np.savetxt(output_file, model_input, header=" ".join(column_names), fmt="%4f")
